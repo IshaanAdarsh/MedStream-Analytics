6dfa1f0694222e809c5e9bef9a0bfc3575f28160
Ishaan Adarsh <ishaanad9@gmail.com>
2023-09-09 18:10:48 +0530

    main-code

--- digital-twin/heart_rate_twin.py
+++
@@ -1,8 +0,0 @@
-class HeartRateMonitorDigitalTwin:
-       def __init__(self, patient_id):
-           self.patient_id = patient_id
-           self.heart_rate = 0
-           self.status = "Normal"
-
-       def update_heart_rate(self, new_rate):
-           self.heart_rate = new_rate
--- main.py
+++
@@ -1,7 +0,0 @@
-from digital_twin.heart_rate_twin import HeartRateMonitorDigitalTwin
-from producer import simulate_heart_rate
-
-if __name__ == "__main__":
-    patient_id = 123  # Replace with an actual patient ID
-    heart_rate_twin = HeartRateMonitorDigitalTwin(patient_id)
-    simulate_heart_rate(heart_rate_twin)
